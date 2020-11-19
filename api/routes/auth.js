d1d3807b553a28a32e5f1b0890248d7048a7c58b
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-11-19 10:35:43 -0500

    Transfering NEXTJS API code to NodeJS

---
+++ api/routes/auth.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get('/login', (req, res) => {
+    res.render('something_here', { user: req.user });
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+// Google Auth
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/profile');
+});
