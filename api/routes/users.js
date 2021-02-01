901ceaf649b5f14f03ff7ee6d1b522848fa7f9ec
Jonathan Laluces <jlalucescareer123@gmail.com>
2021-02-01 12:18:24 -0500

    Feed Route: created

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 const User = require('../../models/User');
+const Moment = require('../../models/Moment');
+
+/* 
+
+    URL: /api/user/...
+
+*/
 
 router.get('/', async (req, res) => {
     User.find({}, function (err, users) {
@@ -8,6 +15,13 @@ router.get('/', async (req, res) => {
             return res.status(400).json({ error: err });
         }
         return res.status(200).json({ users });
+    });
+});
+
+router.get('/feed', async (req, res) => {
+    Moment.find({ public: true }, function (err, moments) {
+        if (err) return res.status(400).json({ error: err });
+        return res.status(200).json({ moments });
     });
 });
 
